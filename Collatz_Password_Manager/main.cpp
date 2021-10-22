4595ebd0478d12ac228568423f79d729145f8c95
Junlin Luo <c1001040@newcastle.ac.uk>
2021-10-22 12:13:32 +0100

    final

--- Collatz_Password_Manager/main.cpp
+++ Collatz_Password_Manager/main.cpp
@@ -8,82 +8,86 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "en_US.UTF-8");
 	while (true) {
-		int choice;
 		cout << "1. Create a new account." << endl;
 		cout << "2. Authenticate the password." << endl;
 		cout << "3. Generate password." << endl;
 		cout << "4. Analyze password." << endl;
 		cout << "5. Quit." << endl;
 		cout << "Your choice:" << endl;
-		cin >> choice;
+		char choice=getchar();
 		switch (choice)
-		{
-		case 1: {
-			string username;
-			wstring password;
-			string temp;
-			cout << "Your username:" << endl;
-			cin >> username;
-			cin.ignore();
-			cout << "Your password:" << endl;
-			getline(wcin, password);
-			User user(username, password);
-			user.save(password_file);
-			break;
-		}
-		case 2: {
-			string username;
-			wstring password;
-			int count = 3;
-			cout << "Your username:" << endl;
-			cin >> username;
-			cin.ignore();
-			while (count != 0) {
+			{
+			case '1': {
+				string username;
+				wstring password;
+				string temp;
+				cout << "Your username:" << endl;
+				cin >> username;
+				cin.ignore();
 				cout << "Your password:" << endl;
 				getline(wcin, password);
 				User user(username, password);
-				try {
-					if (user.authenticate(password_file)) {
-						cout << "success!" << endl;
+				user.save(password_file);
+				break;
+			}
+			case '2': {
+				string username;
+				wstring password;
+				int count = 3;
+				cout << "Your username:" << endl;
+				cin >> username;
+				cin.ignore();
+				while (count != 0) {
+					cout << "Your password:" << endl;
+					getline(wcin, password);
+					User user(username, password);
+					try {
+						if (user.authenticate(password_file)) {
+							cout << "success!" << endl;
+							break;
+						}
+						else {
+							cout << "failure!" << endl;
+							cout << "number of retries: " << count - 1 << endl;
+							count--;
+						}
+					}
+					catch (exception& except) {
+						cout << except.what() << endl;
 						break;
 					}
-					else {
-						cout << "failure!" << endl;
-						cout << "number of retries: " << count-1 << endl;
-						count--;
-					}
 				}
-				catch (exception& except) {
-					cout << except.what() << endl;
-				}
+				break;
 			}
-			break;
+			case '3': {
+				PasswordManager pm;
+				pm.generate_password();
+				AnonymousUsers anon_password(pm.password);
+				AnonymousUsers anon_origin(pm.origin);
+				anon_password.save(password_test_file, pm.password);
+				anon_origin.save(password_origin_file, pm.origin);
+				break;
+			}
+			case '4': {
+				Decoder decoder;
+				string password;
+				cout << "Password: " << endl;
+				cin >> password;
+				vector<string> result = decoder.decrypt(password, false);
+				int i = 0;
+				//vector<string> result=decoder.decrypt(password,true);
+				//decoder.backtracking_combination(0, password);
+				break;
+			}
+			case '5': {
+				exit(1);
+				break;
+			}
+			default: {
+				cin.ignore(numeric_limits<std::streamsize>::max(),'\n');
+				cout << "wrong" << endl;
+				break;
+			}
+			}
 		}
-		case 3: {
-			PasswordManager pm;
-			pm.generate_password();
-			AnonymousUsers anon_password(pm.password);
-			AnonymousUsers anon_origin(pm.origin);
-			anon_password.save(password_test_file,pm.password);
-			anon_origin.save(password_origin_file, pm.origin);
-			break;
-		}
-		case 4: {
-			Decoder decoder;
-			string password;
-			cout << "Password: " << endl;
-			cin >> password;
-			vector<string> result=decoder.decrypt(password,false);
-			break;
-		}
-		case 5: {
-			exit(1);
-			break;
-		}
-		default: {
-			cout << "wrong!" << endl;
-			break;
-		}
-		}
-	}
 }
